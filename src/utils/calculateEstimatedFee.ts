a433a842cef11eb18f62795da9727d1832f0f443
Ming FE <81944309+minglitchfe@users.noreply.github.com>
2023-02-20 18:17:01 +0700

    Update calculateEstimatedFee.ts

--- src/utils/calculateEstimatedFee.ts
+++ src/utils/calculateEstimatedFee.ts
@@ -1,13 +1,11 @@
 import { BUSINESS_FEE, SUBSIDIZE_FLAG } from 'constants/index'
-import { add, subtract } from './numbers'
-import { fromWei, toBN, toWei } from 'web3-utils'
+import { add, mul, subtract } from './numbers'
 
 export const calculateEstimatedFee = (txFee: string, amount: string): string => {
   if (!txFee || !amount) return ''
 
-
   if (SUBSIDIZE_FLAG === 'false') {
-    return `${fromWei(toBN(toWei(amount))) * ((BUSINESS_FEE as unknown as number) / 100)}`
+    return mul(amount, `${((BUSINESS_FEE as unknown as number) / 100)}`)
   }
 
   return add(`${(subtract(amount, txFee) as unknown as number) * ((BUSINESS_FEE as unknown as number) / 100)}`, txFee)
